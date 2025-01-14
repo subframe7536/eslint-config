f00205f70800b2bb0ac574562964b2281f5b4149
subframe7536 <1667077010@qq.com>
2025-01-14 16:04:19 +0800

    feat: simplify jsonc plugin rules

--- src/configs/jsonc.ts
+++ src/configs/jsonc.ts
@@ -16,55 +16,14 @@ export async function jsonc(
     indent = 2,
   } = typeof stylistic === 'boolean' ? {} : stylistic
 
-  const [
-    pluginJsonc,
-    parserJsonc,
-  ] = await Promise.all([
-    interopDefault(import('eslint-plugin-jsonc')),
-    interopDefault(import('jsonc-eslint-parser')),
-  ] as const)
+  const pluginJsonc = await interopDefault(import('eslint-plugin-jsonc'))
 
   return [
-    {
-      name: 'antfu/jsonc/setup',
-      plugins: {
-        jsonc: pluginJsonc as any,
-      },
-    },
+    ...pluginJsonc.configs['flat/recommended-with-jsonc'],
     {
       files,
-      languageOptions: {
-        parser: parserJsonc,
-      },
-      name: 'antfu/jsonc/rules',
+      name: 'subf/jsonc/rules',
       rules: {
-        'jsonc/no-bigint-literals': 'error',
-        'jsonc/no-binary-expression': 'error',
-        'jsonc/no-binary-numeric-literals': 'error',
-        'jsonc/no-dupe-keys': 'error',
-        'jsonc/no-escape-sequence-in-identifier': 'error',
-        'jsonc/no-floating-decimal': 'error',
-        'jsonc/no-hexadecimal-numeric-literals': 'error',
-        'jsonc/no-infinity': 'error',
-        'jsonc/no-multi-str': 'error',
-        'jsonc/no-nan': 'error',
-        'jsonc/no-number-props': 'error',
-        'jsonc/no-numeric-separators': 'error',
-        'jsonc/no-octal': 'error',
-        'jsonc/no-octal-escape': 'error',
-        'jsonc/no-octal-numeric-literals': 'error',
-        'jsonc/no-parenthesized': 'error',
-        'jsonc/no-plus-sign': 'error',
-        'jsonc/no-regexp-literals': 'error',
-        'jsonc/no-sparse-arrays': 'error',
-        'jsonc/no-template-literals': 'error',
-        'jsonc/no-undefined-value': 'error',
-        'jsonc/no-unicode-codepoint-escapes': 'error',
-        'jsonc/no-useless-escape': 'error',
-        'jsonc/space-unary-ops': 'error',
-        'jsonc/valid-json-number': 'error',
-        'jsonc/vue-custom-block/no-parsing-error': 'error',
-
         ...stylistic
           ? {
               'jsonc/array-bracket-spacing': ['error', 'never'],
